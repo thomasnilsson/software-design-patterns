b61b6ed8b90cf08aec2a76f62c91475c474591d7
Thomas Nygaard Nilsson <tnni@netcompany.com>
2020-09-28 21:34:43 +0200

    deleted unused files

--- patterns-course/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("HELLO WORLD!!!!");
-        System.out.println("HELLO WORLD!!!!");
-        System.out.println("HELLO WORLD!!!!");
-        System.out.println("HELLO WORLD!!!!");
-    }
-}
